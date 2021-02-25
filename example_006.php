b9d7021c2f42b3f3b29b30d3ef28172d46542b22
Peter Ma <callofdutypeterma@gmail.com>
2021-02-25 20:38:29 +0800

    First commit on Y520

---
+++ example_006.php
@@ -0,0 +1,145 @@
+<?php
+
+// Include the main TCPDF library (search for installation path).
+require_once('tcpdf_include.php');
+
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Nicola Asuni');
+$pdf->SetTitle('TCPDF Example 006');
+$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+// remove default header/footer
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+
+// Print a table
+$fontname = TCPDF_FONTS::addTTFfont('fonts/kaiu.ttf', 'TrueTypeUnicode');
+$pdf->SetFont($fontname, '', 12, '', false);
+//$pdf->SetFont('msungstdlight', '', 12);
+
+// add a page
+$pdf->AddPage();
+
+$link = mysqli_connect("localhost", "root", "root", "mysys");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$i=0;
+$id = null;
+$jobtitle = null;
+$name = null;
+$semester = null;
+$jobname = null;
+$semnum = null;
+$classname = null;
+$hours = null;
+$subject = null;
+$notes = null;
+
+// Attempt select query execution
+$sql = "SELECT * FROM units";
+if($result = mysqli_query($link, $sql)){
+    
+	while($row = mysqli_fetch_array($result)){
+		$id[$i]=$row['id'];
+		$jobtitle[$i]=$row['jobtitle'];
+		$name[$i]=$row['name'];
+		$semester[$i]=$row['semester'];
+		$jobname[$i]=$row['jobname'];
+		$semnum[$i]=$row['semnum'];
+		$classname[$i]=$row['classname'];
+		$hours[$i]=$row['hours'];
+		$subject[$i]=$row['subject'];
+		$notes[$i]=$row['notes'];
+		$i++;
+    }
+    // Free result set
+    mysqli_free_result($result);
+    
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+ 
+// Close connection
+mysqli_close($link);
+
+// create some HTML content
+$html = '
+<table border="1" align="center">
+	
+	<tr>
+		<td colspan="9" ><font size="16">國立中央大學109學年度  體育室  推薦再聘兼任教研人員名冊</font></td>
+	</tr>
+
+	<tr>
+		<th rowspan="2"><font size="6"><small color="#FFFFFF">small<br></small></font>職稱</th>
+		<th rowspan="2">姓名</th>
+		<th rowspan="2">擬授課學期別</th>
+		<th rowspan="2">專職單位及職稱</th>
+		<th colspan="4">再聘情形</th>
+		<th rowspan="2">備註</th>
+	</tr>
+
+	<tr>
+		<td>授課學期</td>
+		<td>授課名稱</td>
+		<td>每週時數</td>
+		<td>必選修</td>
+	</tr>
+	
+	<tr>
+		<td>'.$jobtitle[0].'</td>
+		<td>'.$name[0].'</td>
+		<td>'.$semester[0].'</td>
+		<td>'.$jobname[0].'</td>
+		<td>'.$semnum[0].'</td>
+		<td>'.$classname[0].'</td>
+		<td>'.$hours[0].'</td>
+		<td>'.$subject[0].'</td>
+		<td>'.$notes[0].'</td>
+	</tr>
+
+	<tr>
+		<td>'.$jobtitle[1].'</td>
+		<td>'.$name[1].'</td>
+		<td>'.$semester[1].'</td>
+		<td>'.$jobname[1].'</td>
+		<td>'.$semnum[1].'</td>
+		<td>'.$classname[1].'</td>
+		<td>'.$hours[1].'</td>
+		<td>'.$subject[1].'</td>
+		<td>'.$notes[1].'</td>
+	</tr>
+
+	<tr>
+		<td colspan="9" align="left">系所主管：                          院長：                             人數：  2人<br>系級教評會 109年 　   月 　   日 108學年度第 2 學期第  次會議審議通過<br>院級教評會 109年 　   月 　   日 108學年度第 2 學期第  次會議報告(審議)通過</td>
+	</tr>
+
+
+</table>
+備註：<br>
+一、請確認貴單位兼任教研人員再聘名單，如需再聘者請於「擬授課學期別」欄位內勾選，並於「再聘情形」欄填具課程資訊，如未開課僅指導研究生論文者，請註明「指導研究生」，本室將依再聘情形核發聘書;不再聘任者，請於備註欄位註明「不予再聘」。<br>
+二、請確認兼任教研人員之專職單位及職稱，如有誤請逕予更正。<br>
+三、依本校三級教評會分工一覽表規定，兼任教研人員之再聘應經系教評會審議通過及院教評會報告，惟如係講授必修課程者，則需經院教評會審議。<br>
+四、本名冊請於109年5月6日前，由各院級單位彙送人事室。<br>
+';
+// output the HTML content
+$pdf->writeHTML($html, true, false, true, false, 'L');
+
+// reset pointer to the last page
+$pdf->lastPage();
+
+//Close and output PDF document
+$pdf->Output('example_006.pdf', 'I');
+
+//============================================================+
+// END OF FILE
+//============================================================+
---
+++ index.php
@@ -0,0 +1,81 @@
+﻿<!DOCTYPE html>
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link href="styles/style.css" rel="stylesheet" type="text/css">
+    <title></title>
+</head>
+<body>
+    <li>Simple PDF with default Header and Footer: [<a href="example_006.php" title="PDF [new window]" target="_blank">PDF</a>]</li>
+    <form action="" method="post">
+        
+    
+        <table border="1" align="center">
+	
+	        <tr>
+		        <td><p>職稱</p><input type="text"  name="jobtitle"></td>
+                <td><p>姓名</p><input type="text" name="name"></td>
+                <td><p>擬授課學期別</p><input type="radio" name="semester" value="1">全學年<input type="radio" name="semester" value="2">上學期<input type="radio" name="semester" value="3">下學期</td>
+                <td><p>專職單位及職稱</p><input type="text" name="jobname"></td>
+                <td>授課學期<input type="number" name="semnum" min="1" max="2"></td>
+                <td>授課名稱(或「指導研究生」)<input type="text" name="classname"></td>
+                <td>每週時數<input type="number" name="hours" min="1" max="4"></td>
+                <td><p>必選修</p><input type="radio" name="subject" value="1">必<input type="radio" name="subject" value="2">選</td>
+	        </tr>
+            
+            <tr>
+                <td colspan="8"><p>備註</p><input type="text" name="notes"></td>
+            </tr>
+
+        </table>
+
+        <p><input type="submit" name="submit" value="送出資料"></p>
+
+
+    </form>
+
+</body>
+</html>
+
+<?php
+
+header("Content-type:text/html;charset=utf-8");
+
+if(isset($_POST['submit'])){
+    
+    $jobtitle = $_POST['jobtitle'];
+    $name = $_POST['name'];
+    $semester = $_POST['semester'];
+    $jobname = $_POST['jobname'];
+    $semnum = $_POST['semnum'];
+    $classname = $_POST['classname'];
+    $hours = $_POST['hours'];
+    $subject = $_POST['subject'];
+    $notes = $_POST['notes'];
+
+    echo "職稱：$jobtitle<br/>";
+    echo "姓名：$name<br/>";
+    echo "擬授課學期別：$semester<br/>";
+    echo "專職單位及職稱：$jobname<br/>";
+    echo "授課學期：$semnum<br/>";
+    echo "授課名稱：$classname<br/>";
+    echo "每週時數：$hours<br/>";
+    echo "必選修：$subject<br/>";
+    echo "備註：$notes<br/>";
+
+    include('connect.php');
+ 
+    //設定連線編碼，防止中文字亂碼
+    $connect->query("SET NAMES 'utf8'");
+ 
+    $insertSql = "INSERT INTO units (jobtitle,name,semester,jobname,semnum,classname,hours,subject,notes) VALUES ('$jobtitle','$name', '$semester','$jobname','$semnum','$classname','$hours','$subject','$notes')";
+    //呼叫query方法(SQL語法)
+    $status = $connect->query($insertSql);
+ 
+    if ($status) {
+        echo '新增成功';
+    } else {
+        echo "錯誤: " . $insertSql . "<br>" . $connect->error;
+    }
+}
